3e1d347af4e23116c34b39a81cad8705e8788103
phaeryl <florian.arnaud2019@gmail.com>
2020-02-18 14:32:56 +0100

    Initial commit (v0.1)

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+}
+
+function create() {
+    this.add.text(400, 300, 'Hello World');
+
+
+}
+
+function update() {
+}
